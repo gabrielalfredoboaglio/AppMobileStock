9b0bed5fd1e86a438a877f964767c00c43915640
Gabriel Alfredo Boaglio <109044049+Atixar@users.noreply.github.com>
2023-05-04 04:54:48 -0300

    Agregar articulo con viewmodels Realizado

---
+++ AppMobileStock/AppMobileStock/ViewModels/ArticuloABMViewModel.cs
@@ -0,0 +1,95 @@
+﻿using AppMobileStock.Models;
+using AppMobileStock.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppMobileStock.ViewModels
+{
+    public class ArticuloABMViewModel : BaseViewModel
+    {
+        #region Properties 
+        public INavigation Navigation { get; set; }
+
+
+        ApiArticuloService articuloService = new ApiArticuloService();
+
+        public ArticuloDTO ArticuloDTO
+        { get => GetPropertyValue<ArticuloDTO>();
+            set => SetPropertyValue(value);
+        }
+
+        public string CodeRead
+        {
+
+
+            get => GetPropertyValue<string>();
+            set => SetPropertyValue(value);
+        }
+
+        public int Numero
+        {
+            get => GetPropertyValue<int>();
+            set => SetPropertyValue(value);
+        }
+
+        #endregion
+
+        #region Constructor
+       public ArticuloABMViewModel()
+        {
+            ArticuloDTO = new ArticuloDTO();    
+            Title = $"Articulos";
+        }
+
+        #endregion
+
+        #region Commands
+        public Command Agregar
+        {
+            get
+            {
+                return new Command(async () => await AgregarArticulo());
+            }
+
+        }
+        #endregion
+
+        #region Methods
+
+        public async Task AgregarArticulo()
+        {
+
+            try
+            {
+                ApiArticuloService apiArticuloService = new ApiArticuloService();
+
+                ArticuloDTO = await apiArticuloService.SendArticulo(ArticuloDTO);
+
+                if (ArticuloDTO.HuboError)
+
+                {
+                    await Application.Current.MainPage.DisplayAlert("Error", "Ocurrio un error " + ArticuloDTO.Mensaje, "Ok");
+
+                }
+                else
+                {
+
+                    await Application.Current.MainPage.DisplayAlert("Exito", ArticuloDTO.Mensaje, "OK");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                await Application.Current.MainPage.DisplayAlert("Error", "Ocurrio un error " + ex.Message, "Ok");
+            }
+        
+
+        }
+        #endregion
+    }
+}
--- AppMobileStock/AppMobileStock/ViewModels/BaseViewModel.cs
+++ AppMobileStock/AppMobileStock/ViewModels/BaseViewModel.cs
@@ -49,6 +49,31 @@ namespace AppMobileStock.ViewModels
 
             changed.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
+        private readonly Dictionary<string, object> _propertyBackingDictionary = new Dictionary<string, object>();
+        protected bool SetPropertyValue<T>(T newValue, [CallerMemberName] string propertyName = null)
+        {
+            if (propertyName == null)
+                throw new ArgumentNullException("propertyName");
+
+            if (EqualityComparer<T>.Default.Equals(newValue, GetPropertyValue<T>(propertyName)))
+                return false;
+
+            _propertyBackingDictionary[propertyName] = newValue;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        protected T GetPropertyValue<T>([CallerMemberName] string propertyName = null)
+        {
+            if (propertyName == null)
+                throw new ArgumentNullException("propertyName");
+
+            object value;
+            if (_propertyBackingDictionary.TryGetValue(propertyName, out value))
+                return (T)value;
+
+            return default(T);
+        }
         #endregion
     }
 }
